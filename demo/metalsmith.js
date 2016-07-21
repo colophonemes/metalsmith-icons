6b36722c9604b667fedd06ada20a0a39eb7a083d
colophonemes <dev@colophonemes.com>
2016-07-21 14:23:22 +0100

    Run demo without cache

--- demo/metalsmith.js
+++ demo/metalsmith.js
@@ -1,12 +1,15 @@
 var Metalsmith = require('../node_modules/metalsmith');
 var icons = require('../lib');
 
+var cache = process.argv.indexOf('--no-cache') > -1 ? false : true;
 
 console.log('Building demo');
 Metalsmith(__dirname)
 .source('./src')
 .destination('./build')
-.use(icons())
+.use(icons({
+	cache: cache ? './.icon_cache' : false,
+}))
 .build(function(err) {
 	if (err) throw err;
 	console.log('Demo build OK');
